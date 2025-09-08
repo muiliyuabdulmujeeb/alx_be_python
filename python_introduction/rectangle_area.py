a580a3435748ff6581848ba4e5c5e32da43149c7
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-09-08 13:24:00 +0100

    intro to python scripts

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,8 @@
+#The fmla for calculating a rectangle is Length * Width where lengh and width has the same unit
+
+length = 10
+width = 5
+
+area = length * width
+
+print("The area of the rectangle is: ", area)
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#fmla for simple interest is s.i= P * R * T, where P is principal in dollars, R is rate in percentage decimal and T is time in years
+
+principal = 1000
+rate = 0.05
+time = 3
+
+simple_interest = principal * rate * time
+
+print("The simple interest is: ", simple_interest)
