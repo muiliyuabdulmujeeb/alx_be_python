45018397f226980b61b0d2eb622faa159c33e732
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-09-15 21:16:19 +0100

    refactor to meet up with checker

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,8 +1,8 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-operand = str(input("Choose the operation (+, -, *, /): "))
+operation = str(input("Choose the operation (+, -, *, /): "))
 
-match operand:
+match operation:
     case "+":
         result = num1 + num2
         print("The result is ", result)
