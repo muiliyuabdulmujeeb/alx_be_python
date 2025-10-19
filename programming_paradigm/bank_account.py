aeca36741ad40806322c5395421c678c86dc2848
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-10-19 13:57:39 +0100

    correction 5

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self, account_balance = 0):
+    def __init__(self, account_balance = 0.00):
         self.account_balance = account_balance
 
     def deposit(self, amount):
@@ -21,4 +21,4 @@ class BankAccount:
             return True
         
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,11 +3,11 @@ def safe_divide(numerator, denominator):
         numerator= float(numerator)
         denominator= float(denominator)
     except ValueError:
-        raise print("Error: Please enter numeric values only.")
+        print("Error: Please enter numeric values only.")
     
     if numerator and denominator:
         try:
             result = numerator/denominator
         except ZeroDivisionError:
-            raise print("Error: Cannot divide by zero.")
+            print("Error: Cannot divide by zero.")
         print(f"The result of the division is {result}")
